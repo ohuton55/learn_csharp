edddb2a405a86b87df43e108a19e262f2139d626
ohuton55 <tttt.tttt.no@gmail.com>
2025-03-08 22:53:11 +0900

    learn dependencies

--- DotNetDependencies/obj/Debug/net8.0/cs.AssemblyInfo.cs
+++ DotNetDependencies/obj/Debug/net8.0/cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dfbfe325dc372a9a9afde3a275fd320439390c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f75bdef351165435c02f1c0616229fe7feb9019")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
