d91a5a7e24422cb3982d16199c2706d225cb9970
ohuton55 <tttt.tttt.no@gmail.com>
2025-03-16 00:06:17 +0900

    learn how to create constructor

---
+++ Classes_M1/BankAccount.cs
@@ -0,0 +1,36 @@
+using System;
+namespace Classes_M1;
+
+public class BankAccount
+{
+    
+     private static int s_nextAccountNumber = 1;
+
+    public int AccountNumber;
+    public double Balance = 0;
+    public static double InterestRate;
+    public string AccountType = "Checking";
+    public readonly string CustomerId;
+
+    static BankAccount()
+    {
+        Random random = new Random();
+        s_nextAccountNumber = random.Next(10000000, 20000000);
+        InterestRate = 0;
+    }
+    
+    public BankAccount(string customerIdNumber)
+    {
+        this.AccountNumber = s_nextAccountNumber++;
+        this.CustomerId = customerIdNumber;
+    }
+    
+    public BankAccount(string customerIdNumber, double balance, string accountType)
+    {
+        this.AccountNumber = s_nextAccountNumber++;
+        this.CustomerId = customerIdNumber;
+        this.Balance = balance;
+        this.AccountType = accountType;
+    }
+
+}
---
+++ Classes_M1/BankCustomer.cs
@@ -0,0 +1,39 @@
+
+ using System;
+
+ namespace Classes_M1;
+
+ public class BankCustomer
+ {
+     // add public fields for FirstName, LastName, and CustomerId
+     private static int s_nextCustomerId;
+     public string FirstName = "Tim";
+     public string LastName = "Shao";
+     public readonly string CustomerId;
+    
+    
+    static BankCustomer()
+    {
+        Random random = new Random();
+        s_nextCustomerId = random.Next(10000000, 20000000);
+    }
+
+     public BankCustomer()
+     {
+        this.CustomerId = (s_nextCustomerId++).ToString("D10");
+     }
+    
+     public BankCustomer(string firstName, string lastName)
+     {
+         FirstName = firstName;
+         LastName = lastName;
+         this.CustomerId = (s_nextCustomerId++).ToString("D10");
+     }
+    
+     public BankCustomer(string firstName, string lastName, string customerIdNumber)
+     {
+         FirstName = firstName;
+         LastName = lastName;
+         CustomerId = customerIdNumber;
+     }
+ }
